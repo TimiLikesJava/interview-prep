857bc71d15b69b55de18840f483715be2adf704e
Timi Olaosebikan <timilaosebikan@gmail.com>
2020-06-16 10:25:37 -0500

    merge two sorted arrays

---
+++ leetcode/merge.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        last = len(nums1) - 1
+        
+        m -= 1
+        n -= 1
+        
+        while(last >= 0):
+            if(m < 0):
+                nums1[last] = nums2[n]
+                n -= 1
+            elif(n < 0):
+                nums1[last] = nums1[m]
+                m -= 1
+                
+            else:
+                if(nums2[n] > nums1[m]):
+                    nums1[last] = nums2[n]
+                    n -= 1
+                else:
+                    nums1[last] = nums1[m]
+                    m -= 1
+                    
+            last -= 1
