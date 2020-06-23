e4931c3d15da3837ecc0ad49fb242257757d666a
Timi Olaosebikan <timilaosebikan@gmail.com>
2020-06-23 15:59:20 -0500

    officially a swe

---
+++ leetcode/reverseList.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while(curr is not None):
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        head = prev
+        return prev
